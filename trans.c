efa7e3e7fe4117d3927b017e9bd029a67aa49f9e
JuuDyung <jsleeyh2002@naver.com>
2018-11-13 13:57:22 +0900

    second

--- trans.c
+++ trans.c
@@ -16,6 +16,36 @@ int instr_trans(char *op, char *args, char* mcode)
 	/*    generate machine code     */
     /*  							*/
 	/********************************/
-	
+	int type;
+	char opcode[10];
+	swtch(type){
+		case 1:{//reg to reg
+			strcpy(opcode,"89");
+			break;
+		}
+		case 2:{//mem to reg sib
+			strcpy(opcode,"8b");
+			break;
+		}
+		case 3:{//mem to reg dis
+			strcpy(opcode,"8b");
+			break;
+		}
+		case 4:{//mem to reg eax
+			strcpy(opcode,"a1");
+			break;
+		}
+		case 5:{//reg to mem
+			strcpy(opcode,"a3");
+			break;
+		}
+		case 6:{//immediate to reg
+			strcpy(opcode,"b8");
+			break;
+		}
+		default:{
+			strcpy(opcode,"ERR");
+			break;
+		}
 	return 1;	
 }
